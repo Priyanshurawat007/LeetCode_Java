4e422b37881fa67f4f7229326ee22ef3aff52be4
Priyanshu Rawat <100402236+Priyanshurawat007@users.noreply.github.com>
2024-05-18 20:52:40 +0530

    Create DistributeCoins.java

---
+++ DistributeCoins.java
@@ -0,0 +1,18 @@
+class Solution {
+    private int moves;
+
+    private int traverse(TreeNode node) {
+        if (node == null) return 0;
+        int leftExcess = traverse(node.left);
+        int rightExcess = traverse(node.right);
+        int totalExcess = node.val + leftExcess + rightExcess - 1;
+        moves += Math.abs(totalExcess);
+        return totalExcess;
+    }
+
+    public int distributeCoins(TreeNode root) {
+        moves = 0;
+        traverse(root);
+        return moves;
+    }
+}
