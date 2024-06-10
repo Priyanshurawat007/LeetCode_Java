f072116bb5781e49eb1c2bb2a7e1f2843324babf
Priyanshu Rawat <100402236+Priyanshurawat007@users.noreply.github.com>
2024-06-10 20:41:57 +0530

    Create ReverseLinkedList.java

---
+++ ReverseLinkedList.java
@@ -0,0 +1,20 @@
+public class Solution 
+{
+    public static LinkedListNode<Integer> reverseLinkedList(LinkedListNode<Integer> head) 
+    {
+        LinkedListNode<Integer> prev = null;
+        LinkedListNode<Integer> current = head;
+        LinkedListNode<Integer> next = null;
+        
+        while (current != null) 
+        {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        
+        head = prev;
+        return head;
+    }
+}
